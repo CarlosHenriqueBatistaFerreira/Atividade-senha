34c614d894c058482694c3ca0d3ccf330c2d975c
Carlos Ferreira <161725212+CarlosHenriqueBatistaFerreira@users.noreply.github.com>
2024-08-30 14:34:05 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha incorreta informada, escrever a mensagem "Acesso Negado". Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo deve ser encerrado. Considere que a senha correta é 2024;
+const prompt = require('prompt-sync');
+const senhaCorreta = 2024;
+var senha = 2024;
+
+while (senha != senha);
+console.log('Acesso Negado');
+entrada = prompt('Digite a senha: ');
+console.log('Acesso Permitido');
+
+if (senha != 2024) {
+  console.log('Acesso Permitido');
+}
+else {
+  console.log('Acesso Negado');
+}
